85a9e768eec3dc08ec5ae01b1f42cc2cadf8ccd9
Allan Kiche <tle2819@students.mu.ac.ke>
2023-01-13 22:37:25 +0300

    readme file

---
+++ Arrays/73-Set-Matrix-Zeros.py
@@ -0,0 +1,9 @@
+def set_zeros(matrix):
+    
+    m, n = len(matrix), len(matrix[0])
+    first_row_has_zero = False
+    first_col_has_zero = False
+    
+    #check if the first row has zero
+    
+    for j in range r
---
+++ Arrays/README.md
@@ -0,0 +1,1 @@
+#th above line defines a function called called set_zeros tha takes a single argument called matrix and represents the input matrix we want to modify
