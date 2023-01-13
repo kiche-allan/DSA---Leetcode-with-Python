c3999ccfe3f6ab22bc5d89121bd07f459e228b0d
Allan Kiche <tle2819@students.mu.ac.ke>
2023-01-13 22:49:08 +0300

    setzeros

--- Arrays/73-Set-Matrix-Zeros.py
+++ Arrays/73-Set-Matrix-Zeros.py
@@ -1,9 +1,32 @@
-def set_zeros(matrix):
-    
+def set_zeros(matrix): 
     m, n = len(matrix), len(matrix[0])
     first_row_has_zero = False
     first_col_has_zero = False
     
     #check if the first row has zero
     
-    for j in range r
+    for j in range (n):
+        if matrix [0][j] == 0:
+            first_row_has_zero = True
+            break
+        
+    #check if the first column has zero
+    
+    for i in range(m):
+        if matrix[i][0] == 0:
+            first_col_has_zero = True
+            break
+        
+    #check for zeos in the rest of the matrix
+    
+    for i in range(1, m):
+        for j in range(1, n):
+            if matrix[i][j] == 0:
+                matrix[i][0]= 0
+                matrix[0][j] = 0
+                
+    #se the zeros in the first row and column if needed
+    
+    if first_row_has_zero:
+        for j in range(n):
+            matrix[0][j] = 0
